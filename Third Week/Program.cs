2924c8b4265fece06b97de53b64ba4e75485ba95
Owoeye Moyosore <moyosoreowoeye2000@gmail.com>
2022-04-11 14:53:34 +0100

    Added stuff

---
+++ Third Week/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+namespace Generic
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Addition<int, double> sum = new Addition<int, double>();// or var sum = new Addition<int, double>();
+            sum.Add(100, 89.4, 42);
+            // Generic interface declaration
+            var data = new Data<object>();
+            data.Add(1);
+            data.Add(true);
+            data.Add('\n');
+
+            
+
+        }
+
+    }
+
+    public class Addition<T, R>
+    {
+        public void Add(T parameter1, R parameter2, int p3)
+        {
+
+        }
+    }
+
+    //Base class Generic
+    public class Shape<P>
+    {
+        public P Radius { get; set; }
+
+    }
+
+    public class Circle<P> : Shape<P>
+    {
+        public Circle(P p1)
+        {
+
+        }
+    }// end of base class generic
+
+    //Generic Interface
+    public interface IData<D>
+    {
+        void Add(D item);
+
+    }
+    public class Data<D> : IData<D>
+    {
+        private D[] _data;
+        private int _count;
+
+        public Data()
+        {
+            _data = new D[10];
+        }
+        public void Add(D item)
+        {
+            _data[_count++] = item;
+        }
+    }
+
+    public class IShape
+    {
+        public class Square : IShape
+        {
+            private Square()
+            {
+
+            }
+        }
+        public class Circle
+        {
+            public Circle()
+            {
+
+            }
+        }
+
+        public class Shape<U, T> where U : new() where T : struct
+        {
+
+        }
+        public class Triangle : Shape<Circle>
+        {
+
+        }
+    }
+
+    
+}
+
+
---
+++ shoppingbag/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Shopping
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            ShoppingBag<Bag> shoppingBag = new ShoppingBag<Bag>();
+            shoppingBag.AddItem(new Bag() { ProductName = "Noodles ", ProductCount = 80 });
+            shoppingBag.AddItem(new Bag() { ProductName = "Clothes", ProductCount = 56 });
+            shoppingBag.AddItem(new Bag() { ProductName = "Rice", ProductCount = 10 });
+            shoppingBag.AddItem(new Bag() { ProductName = "Hypo", ProductCount = 35 });
+
+            var counter = 0;
+            while (counter <10)
+            {
+                Console.WriteLine(shoppingBag.GetItem(counter));
+                counter++;
+            }
+        }
+    }
+    public class ShoppingBag<T> where T : Bag
+    {
+        private T[] _items = new T[10];
+        private int _index = 0;
+
+        public void AddItem(T item)
+        {
+            _items[_index++] = item;
+        }
+
+
+        public T GetItem(int index)
+        {
+            return _items[index];
+        }
+       
+    }
+    public class Bag
+    {
+        public string ProductName { get; set; }
+        public int ProductCount { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ProductName} - {ProductCount}";
+        }
+
+
+    }
+
+
+    
+}
